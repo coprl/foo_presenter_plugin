0f3787a51ec931abb1c3083df24e245897dd10f7
Rx <russell@russelledens.net>
2021-06-01 14:27:27 -0400

    BREAKING CHANGE: Changes to support COPRL 3.0 with rails native views. * The templates now need to be rendered from an application directory. * The view directory is returned from a new callback method. This allows the CORPL rails engine controller to add the view directory to the views path before calling the render method.

--- lib/coprl/presenters/plugins/foo/web_client/components/foo_render.rb
+++ lib/coprl/presenters/plugins/foo/web_client/components/foo_render.rb
@@ -3,18 +3,22 @@ module Coprl
     module Plugins
       module Foo
         module WebClientComponents
+
+          def view_dir_foo(pom)
+            File.join(__dir__, '../../../../../../..', 'views', 'components')
+          end
+
           def render_foo(comp,render:, components:, index:)
-            view_dir = File.join(__dir__, '../../../../../../..', 'views', 'components')
-            render.call :erb, :foo, views: view_dir,
+            render.call :erb, 'foo',
+                        views: view_dir_foo(comp),
                         locals: {comp: comp,
                         components: components, index: index}
           end
           # The string returned from this method will be added to the HTML header section of the page layout
           # It will be called once for the page.
           # The pom is passed along with the sinatra render method.
-          def render_header_foo(_pom, render:)
-            view_dir = File.join(__dir__, '../../../../../../..', 'views', 'components')
-            render.call :erb, :foo_header, views: view_dir
+          def render_header_foo(pom, render:)
+            render.call :erb, 'foo_header' , views: view_dir_foo(pom)
           end
         end
       end
